a4212da4d26d28277c09c9f1634fe2829922a568
blindbanditbmc <72346625+blindbanditbmc@users.noreply.github.com>
2022-11-29 00:29:12 +0300

    character limit and counter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -88,3 +88,14 @@ jQuery( document ).ready(function( $ ) {
             });
         }
 });
+$('textarea').keyup(function() {
+    
+    var characterCount = $(this).val().length,
+        current = $('#current'),
+        maximum = $('#maximum'),
+        theCount = $('#the-count');
+      
+    current.text(characterCount);
+   
+    
+});
